4eba4de41d431e237391c287635c57f89dd6dcf9
Nguyen Toan Thinh <thinh.nguyentoan@vietis.vn>
2017-08-11 10:29:17 +0700

    Hello world

---
+++ XAMLBasic/XAMLBasic/HelloXamlPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XAMLBasic
+{
+    public partial class HelloXamlPage
+    {
+        public HelloXamlPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OpenXAMLCodePage(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new XamlPlusCodePage());
+        }
+    }
+}
